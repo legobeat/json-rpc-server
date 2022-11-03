02366db54e9495b83f1a662aa86a59695c986eed
Arham Jain <arham.nec@gmail.com>
2022-11-03 11:58:00 +0530

    feat(ip): keeping ipv6 padding as is

--- src/middlewares/injectIP.ts
+++ src/middlewares/injectIP.ts
@@ -5,13 +5,7 @@ const util = require('util')
 const CONFIG = require('../config')
 
 const injectIP = (req: any, res:any, next: Function) => {
-    if (req.body.method === 'eth_sendRawTransaction' && CONFIG.recordTxStatus) {
-        let ip = req.ip
-        if (ip.substr(0, 7) == '::ffff:') {
-          ip = ip.substr(7)
-        }
-        req.body.params[1000] = ip
-    }
+    if (req.body.method === 'eth_sendRawTransaction' && CONFIG.recordTxStatus) req.body.params[1000] = req.ip
     next()
     return 
 }
