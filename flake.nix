aa3d9bd6535fe00506a5e918228dd21c4ade4e96
municorn <municorn@musicaloft.com>
2023-06-21 18:54:04 -0500

    flake: Upgrade NodeJS to 18.16.0

--- flake.nix
+++ flake.nix
@@ -17,12 +17,16 @@
       (system: let
         pkgs = import nixpkgs {
           inherit system;
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (nixpkgs.lib.getName pkg) [
+              "vscode"
+            ];
         };
         buildNodeJs = pkgs.callPackage "${nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {python = pkgs.python3;};
         custom-nodejs = buildNodeJs {
           enableNpm = true;
-          version = "16.11.1";
-          sha256 = "0y32mdv8zs35la2bny8d9rxjvj1vr8z079ji1g6ajc2yw96pyn37";
+          version = "18.16.0";
+          sha256 = "067ijdh9073wyvqb7rlxjy8lbl08j0hg5954vnd50ni37qiimn1k";
         };
 
         nativeBuildInputs = with pkgs; [
@@ -31,7 +35,6 @@
         buildInputs = with pkgs; [];
       in {
         # TODO `nix build`
-
 
         # TODO `nix run`
         defaultApp = utils.lib.mkApp {
