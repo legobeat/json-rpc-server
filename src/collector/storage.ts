cd58aa36230765567be658e368da27d88403e882
kgmyatthu <2962000kaungmyatthu@gmail.com>
2023-10-07 14:26:58 +0700

    feat: sqlite3adapter

--- src/collector/storage.ts
+++ src/collector/storage.ts
@@ -2,36 +2,63 @@ import Database from 'better-sqlite3'
 import fs from 'fs'
 import IDX_COLLECTOR_CONFIG, {DB} from './config'
 
-export let db: any = initDatabase()
-
-function sqlite3Adapter(path: string): any {
-  // create path if not exist recursively
-  const dir = path.substring(0, path.lastIndexOf('/'))
-  console.log(dir)
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, {recursive: true})
-  }
-  db = new Database(path)
-  db.pragma('journal_mode = WAL')
-  console.log('Database initialized.')
-  return db
-}
-
-function postgresAdapter(): any {
-  // potential implement in future
-}
-
-function initDatabase(): void {
-  console.log(IDX_COLLECTOR_CONFIG);
+export const db: Sqlite3Adapter | PostgresAdapter = (()=>{
   if (IDX_COLLECTOR_CONFIG.database.type === DB.SQLITE) {
-    return sqlite3Adapter(IDX_COLLECTOR_CONFIG.database.disk_path)
+    return new Sqlite3Adapter(IDX_COLLECTOR_CONFIG.database.disk_path)
   }
   else if (IDX_COLLECTOR_CONFIG.database.type === DB.POSTGRESQL) {
-    return postgresAdapter()
+    return new PostgresAdapter(
+      IDX_COLLECTOR_CONFIG.database.username, 
+      IDX_COLLECTOR_CONFIG.database.password, 
+      IDX_COLLECTOR_CONFIG.database.host, 
+      IDX_COLLECTOR_CONFIG.database.port, 
+      IDX_COLLECTOR_CONFIG.database.name
+    )
   }
   else{
     throw new Error('Database type not supported.')
   }
+})();
+
+class Sqlite3Adapter{
+  db: any
+
+  constructor(path: string) {
+    let db = null;
+    const dir = path.substring(0, path.lastIndexOf('/'))
+    console.log(dir)
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {recursive: true})
+    }
+    db = new Database(path)
+    db.pragma('journal_mode = WAL')
+    console.log('Database initialized.')
+    this.db = db
+  }
+
+  exec(sql: string): void{
+    this.db.exec(sql)
+  }
+
+  prepare(sql: string): any {
+    return this.db.prepare(sql)
+  }
+
 }
 
+class PostgresAdapter{
+  db: never
 
+  constructor(username: string, password: string, host: string, port: number, database: string) {
+    throw new Error('Not implemented.')
+  }
+
+  exec(sql: string): void{
+    throw new Error('Not implemented.')
+  }
+
+  prepare(sql: string): any{
+    throw new Error('Not implemented.')
+  }
+
+}
